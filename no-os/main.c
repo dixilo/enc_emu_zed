462d88029d947c728715eeb6e031911284348e0f
Kyoto CMB <kucmb.group@gmail.com>
2020-07-02 15:22:04 +0900

    software update

--- no-os/main.c
+++ no-os/main.c
@@ -1,67 +1,165 @@
-/******************************************************************************
-*
-* Copyright (C) 2009 - 2014 Xilinx, Inc.  All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* Use of the Software is limited solely to applications:
-* (a) running on a Xilinx device, or
-* (b) that interact with a Xilinx device through a bus or interconnect.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
-* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*
-* Except as contained in this notice, the name of the Xilinx shall not be used
-* in advertising or otherwise to promote the sale, use or other dealings in
-* this Software without prior written authorization from Xilinx.
-*
-******************************************************************************/
-
-/*
- * helloworld.c: simple test application
- *
- * This application configures UART 16550 to baud rate 9600.
- * PS7 UART (Zynq) is not initialized by this application, since
- * bootrom/bsp configures it to baud rate 115200
- *
- * ------------------------------------------------
- * | UART TYPE   BAUD RATE                        |
- * ------------------------------------------------
- *   uartns550   9600
- *   uartlite    Configurable only in HW design
- *   ps7_uart    115200 (configured by bootrom/bsp)
- */
-
 #include <stdio.h>
 #include "platform.h"
 #include "xil_printf.h"
 #include "xparameters.h"
 #include "xil_io.h"
+#include "xuartlite.h"
 #include "sleep.h"
 
+#define POLARITY_MASK 0x00000002
+#define Z_PULSE_MASK 0x00000004
+#define SPEED_MASK 0xFFFFFFF8
+#define UART_DEV_ID XPAR_AXI_UARTLITE_DEVICE_ID
+
+
+int set_pol(int pol){
+	int cur_status;
+	cur_status = Xil_In32(XPAR_AXI_GPIO_BASEADDR);
+	Xil_Out32(XPAR_AXI_GPIO_BASEADDR, (cur_status & (~POLARITY_MASK)) | pol);
+	return 0;
+}
+
+int set_neg_inc(){
+	set_pol(3);
+	return 0;
+}
+
+int set_pos_inc(){
+	set_pol(1);
+	return 0;
+}
+
+int stop(){
+	set_pol(0);
+	return 0;
+}
+
+int set_speed(int speed){
+	int cur_status;
+	cur_status = Xil_In32(XPAR_AXI_GPIO_BASEADDR);
+	Xil_Out32(XPAR_AXI_GPIO_BASEADDR, (cur_status & (~SPEED_MASK)) | (speed << 3));
+
+	return 0;
+}
+
+int set_speed_dialog(){
+	int speed;
+	int ret;
+
+	print("PLEASE PUT SPEED: ");
+	ret = scanf("%d", &speed);
+
+	if (ret == EOF){
+		print("INVALID INPUT.\n\r");
+		return -1;
+	}
+
+	set_speed(speed);
+	return 0;
+}
+
+int send_z_pulse(){
+	int cur_status;
+	cur_status = Xil_In32(XPAR_AXI_GPIO_BASEADDR);
+	Xil_Out32(XPAR_AXI_GPIO_BASEADDR, cur_status  | Z_PULSE_MASK);
+	Xil_Out32(XPAR_AXI_GPIO_BASEADDR, cur_status & (~Z_PULSE_MASK));
+
+	return 0;
+}
+
+
+int send_uart_dialog(XUartLite *inst){
+	u8 uart_str[16];
+	int ret;
+
+	print("PLEASE PUT UART STRING: ");
+	ret = scanf("%s", uart_str);
+	if (ret == EOF){
+		print("INVALID INPUT.\n\r");
+		return -1;
+	}
+
+	XUartLite_Send(inst, uart_str, sizeof(uart_str));
+
+	return 0;
+}
 
 int main()
 {
+	int menu_num;
+	int ret;
+	XUartLite* uart_lite;
+
+	// Platform initialization
     init_platform();
+    print("###### ENCODER EMULATOR v0.1 ######\n\r");
 
-    print("Hello World\n\r");
-
+    // GPIO initialization
+    print("## GPIO RESET ##\n\r");
     Xil_Out32(XPAR_AXI_GPIO_BASEADDR, 0x00000000);
-    usleep(100);
-    Xil_Out32(XPAR_AXI_GPIO_BASEADDR, 0x00000105);
+    print("## GPIO RESET SUCCESS ##\n\r");
+
+    // UART initialization
+    print("## UART INIT ##\n\r");
+
+    ret = XUartLite_Initialize(uart_lite, UART_DEV_ID);
+	if (ret != XST_SUCCESS){
+		print("## UART INIT FAIL.##\n\r");
+		return XST_FAILURE;
+	}
+
+	ret = XUartLite_SelfTest(uart_lite);
+	if (ret != XST_SUCCESS){
+		print("## UART SELF TEST FAIL.##\n\r");
+		return XST_FAILURE;
+	}
+
+	print("## UARTRESET SUCCESS ##\n\r");
+
+    // Main menu
+    while(1){
+    	print("###### MENU ######\n\r");
+    	print("1. POSITIVE INCREMENT\n\r");
+    	print("2. NEGATIVE INCREMENT\n\r");
+    	print("3. SET SPEED\n\r");
+    	print("4. SEND Z-PULSE\n\r");
+    	print("5. SEND UART\n\r");
+    	print("6. STOP\n\r");
+    	print("PLEASE SELECT MENU NUM: ");
+
+    	ret = scanf("%d", &menu_num);
+
+    	if ( ret == EOF ){
+    		print("INVALID INPUT.\n\r");
+    		continue;
+    	}
+
+    	switch(menu_num){
+    	case 1:
+    		set_pos_inc();
+    		break;
+    	case 2:
+    		set_neg_inc();
+    		break;
+    	case 3:
+    		set_speed_dialog();
+    		break;
+    	case 4:
+    		send_z_pulse();
+    		break;
+    	case 5:
+    		send_uart_dialog(uart_lite);
+    		break;
+    	case 6:
+    		stop();
+    		break;
+    	default:
+    		print("INVALID INPUT.\n\r");
+    		break;
+    	}
+    	print("DONE.\n\r");
+    }
+
 
     cleanup_platform();
     return 0;
